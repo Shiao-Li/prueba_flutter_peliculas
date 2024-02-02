e395214494a23cdbc7940431382ae036e9d232ca
Shiao-Li <roberto.shiao@epn.edu.ec>
2024-02-02 16:33:00 -0500

    init

---
+++ lib/constants/api_constants.dart
@@ -0,0 +1,3 @@
+const String TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
+const String TMDB_API_KEY = "4e00dfaa6b36343bfb15b4b6d925e36f";
+const String TMDB_BASE_IMAGE_URL = "https://image.tmdb.org/t/p/";
